8d4449802e88537f29704c333bbf2c487c27c4e1
AJ Walter <aj.walter@aol.com>
2016-09-04 23:40:26 -0500

    Add basic log test.

--- css/style.php
+++ css/style.php
@@ -6,13 +6,13 @@
 	Note: the //esc: parts of the php tags in the CSS file
 	fix the parsing that Notepad++ uses to display CSS.
 	
-	Note 2: PHP variables are used when you need to update
+	Note: PHP variables are used when you need to update
 	values that appear in multiple places. For example, editing
 	the footer might require you to change a value in 3 
-	different places
+	different places to keep the same layout.
 	*/
 	header("Content-type: text/css; charset: UTF-8");
-	$LOG_BAR_HEIGHT = "70px";
+	$LOG_BAR_HEIGHT = "90px";
 	$NAV_LINK_HEIGHT = "50px";
 	include "style.css";
 ?>
---
+++ edit.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+	<title>Rubric Pro</title>
+	<link rel="stylesheet" href="css/style.php"> 
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Ubuntu+Mono" rel="stylesheet"> 
+	<meta charset="UTF-8">
+	<meta name="author" content="Aaron Walter (2016)">
+	<meta name="description" content="Edit your class rubrics and track progress with Rubric Pro!">
+</head>
+<body>
+	<div id="content">
+		<div id="sidebar">
+			<img id="logo" src="images/logo.png" alt="Rubric Pro">
+			<div id="navigation">
+				<a href="#"><span>Dashboard</span></a>
+				<a href="#"><span>Classes</span></a>
+				<a href="#"><span>Students</span></a>
+				<a href="#"><span>Rubrics</span></a>
+				<a href="#"><span>Catigories</span></a>
+				<a href="#"><span>Settings</span></a>
+			</div>
+		</div>
+	</div>
+	<div id="logbar">
+		<div id="console">
+			[18:02:31] [Server thread/INFO]: Starting minecraft server version 1.10<br>
+			[18:02:32] [Server thread/INFO]: Loading properties<br>
+			[18:02:32] [Server thread/INFO]: Default game type: SURVIVAL<br>
+			[18:02:32] [Server thread/INFO]: Generating keypair<br>
+			[18:02:32] [Server thread/INFO]: Starting Minecraft server on *:25566<br>
+			[18:02:32] [Server thread/INFO]: Using default channel type<br>
+			[18:02:32] [Server thread/WARN]: **** SERVER IS RUNNING IN OFFLINE/INSECURE MODE!<br>
+			[18:02:32] [Server thread/WARN]: The server will make no attempt to authenticate usernames. Beware.<br>
+			[18:02:32] [Server thread/WARN]: While this makes the game possible to play without internet access, it also opens up the ability for hackers to connect.<br>
+			[18:02:32] [Server thread/WARN]: To change this, set "online-mode" to "true" in the server.properties file.<br>
+			[18:02:32] [Server thread/INFO]: Preparing level "world"<br>
+			[18:02:33] [Server thread/INFO]: Preparing start region for level 0<br>
+			[18:02:34] [Server thread/INFO]: Preparing spawn area: 0%<br>
+			[18:02:35] [Server thread/INFO]: Preparing spawn area: 39%<br>
+			[18:02:36] [Server thread/INFO]: Preparing spawn area: 85%<br>
+			[18:02:36] [Server thread/INFO]: Done (3.943s)! For help, type "help" or "?"<br>
+		</div>
+	</div>
+</body>
