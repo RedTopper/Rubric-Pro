c7e7f2c28a2a8f3ef4f46ecd5f9d8eab757e7e97
AJ Walter <aj.walter@aol.com>
2016-09-04 23:19:34 -0500

    Begin backbone work of website.

---
+++ css/style.php
@@ -0,0 +1,18 @@
+<?php
+	/*
+	This is a configuration file for the CSS stylesheet.
+	You can change settings here.
+	
+	Note: the //esc: parts of the php tags in the CSS file
+	fix the parsing that Notepad++ uses to display CSS.
+	
+	Note 2: PHP variables are used when you need to update
+	values that appear in multiple places. For example, editing
+	the footer might require you to change a value in 3 
+	different places
+	*/
+	header("Content-type: text/css; charset: UTF-8");
+	$LOG_BAR_HEIGHT = "70px";
+	$NAV_LINK_HEIGHT = "50px";
+	include "style.css";
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+	<title>Rubric Pro</title>
+	<link rel="stylesheet" href="css/style.php"> 
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"> 
+	<meta charset="UTF-8">
+	<meta name="author" content="Aaron Walter (2016)">
+	<meta name="description" content="Edit your class rubrics and track progress with Rubric Pro!">
+</head>
+<body>
+	<div id="content">
+		<div id="sidebar">
+			<img id="logo" src="images/logo.png" alt="Rubric Pro">
+			<div id="navigation">
+				<a href="#"><span>Dashboard</span></a>
+				<a href="#"><span>Classes</span></a>
+				<a href="#"><span>Students</span></a>
+				<a href="#"><span>Rubrics</span></a>
+				<a href="#"><span>Catigories</span></a>
+				<a href="#"><span>Settings</span></a>
+			</div>
+		</div>
+	</div>
+	<div id="logbar">
+		<div id="console">
+			text<br>text<br>text<br>text<br>text<br>text<br>text<br>
+		</div>
+	</div>
+</body>
