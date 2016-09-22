e6846dbe6f6ffe1350f8a923fba38719ac30205d
AJ Walter <aj.walter@aol.com>
2016-09-21 21:34:07 -0500

    Editor for classes, such as binding students or creation.

---
+++ backend/classes_create_submit.php
@@ -0,0 +1,48 @@
+<?php
+$needsAuthentication = true;
+$needsAJAX = true;
+$needsTeacher = true;
+include "db.php";
+$NAME = isset($_POST["NAME"]) ? $_POST["NAME"] : "";
+$YEAR = isset($_POST["YEAR"]) ? $_POST["YEAR"] : "";
+$TERM = isset($_POST["TERM"]) ? $_POST["TERM"] : "";
+$PERIOD = isset($_POST["PERIOD"]) ? $_POST["PERIOD"] : "";
+$DESCRIPTOR = isset($_POST["DESCRIPTOR"]) ? $_POST["DESCRIPTOR"] : "";
+
+#Class name needs to be 2 or more chars.
+if(strlen($NAME) < 2) {
+	showError("Error creating class!", "The class name is too short.", "Please type a longer class name.", 400);
+}
+
+if(strlen($PERIOD) < 2) {
+	showError("Error creating class!", "The defined period is too short.", "Please type a longer period name.", 400);
+}
+
+if(!is_numeric($YEAR)) {
+	showError("Error creating class!", "The year needs to be numerical.", "Please type a number for the year.", 400);
+}
+
+if(!is_numeric($TERM)) {
+	showError("Error creating class!", "The term needs to be numerical.", "Please type a number for the term.", 400);
+}
+
+$stmt = $conn->prepare(
+<<<SQL
+INSERT INTO CLASS
+(TEACHER_NUM, NAME, YEAR, PERIOD, TERM, DESCRIPTOR)
+VALUES
+(:teacherID, :classname, :year, :period, :term, :descriptor)
+SQL
+);
+$stmt->execute(array('teacherID' => $_SESSION["NUM"],
+					 'classname' => $NAME,
+					 'year' => $YEAR,
+					 'period' => $PERIOD,
+					 'term' => $TERM,
+					 'descriptor' => $DESCRIPTOR));	
+
+#Redirect using some Javascript
+header("JS-Redirect: classes");
+
+showError("Ok!", "The class has been created.", "We'll automatically redirect you now...", 201);
+?>
---
+++ backend/classes_edit.php
@@ -0,0 +1,48 @@
+<?php
+$needsAuthentication = true;
+$needsAJAX = true;
+$needsTeacher = true;
+include "db.php";
+$CLASS = isset($_POST["CLASS"]) ? $_POST["CLASS"] : "";
+
+$stmt = $conn->prepare(
+<<<SQL
+SELECT NUM, TEACHER_NUM, NAME, YEAR, TERM, PERIOD, DESCRIPTOR
+FROM CLASS
+WHERE
+TEACHER_NUM = :teacherID AND NUM = :class
+SQL
+);
+$stmt->execute(array('teacherID' => $_SESSION["NUM"],
+					 'class' => $CLASS));	
+$count = $stmt->rowCount();
+
+#Check to see if there is really a student!
+if($count == 1) {
+	$row = $stmt->fetch();
+?>
+	<div class="object subtitle">
+		<h2>What would you like to do with <?php echo htmlentities($row["NAME"]); ?>?</h2>
+	</div>
+	<a id="js_classes_edit_students" class="object create" href="#"><div class="arrow"></div>
+		<h1>Add a student to this class</h1>
+	</a>
+	<a id="js_classes_edit_students" class="object create" href="#"><div class="arrow"></div>
+		<h1>Create a project for this class</h1>
+	</a>
+	<div class="object subtitle">
+		<h2>Class information: </h2>
+	</div>
+	<div class="object subtext">
+		<p><?php echo "Year: " . $row["YEAR"]; ?>.
+		<p><?php echo "Term: " . $row["TERM"]; ?>.
+		<p><?php echo htmlentities($row["PERIOD"]); ?>.
+		<p><?php echo ($row["DESCRIPTOR"] !== "" ? 
+			"Description: " . htmlentities($row["DESCRIPTOR"]) . "." : 
+			"");  ?>
+	</div>
+<?php
+} else {
+		showError("Whoops!", "Something went wrong when requesting that class.", "Check to see if you are the owner of that class, or if your client sent the wrong class ID.", 400);
+}
+?>
